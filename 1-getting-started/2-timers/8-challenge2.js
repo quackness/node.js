3cb0cc76c1f161c38866c46c86600b6c31b01e16
Karolina Redden <dubajkaro@gmail.com>
2023-03-20 22:03:05 -0300

    setInterval challenge solved

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -1,5 +1,18 @@
 // Print "Hello World"
+
 // Every second
 // And stop after 5 times
 
+
 // After 5 times. Print "Done" and let Node exit.
+
+let counter  = 0 
+let counting = setInterval(() => {
+  console.log('Hello World');
+  counter += 1;
+
+  if (counter === 5) {
+    console.log('Done');
+    clearInterval(counting);
+  }
+}, 1000);
