c1c55774805b070411ca1a40eb5ceefd330a3326
Karolina Redden <dubajkaro@gmail.com>
2023-03-22 20:46:52 -0300

    add files

--- 2-modern-js/1-vars.js
+++ 2-modern-js/1-vars.js
@@ -6,12 +6,12 @@ if (true) {
   // Block Scope
 }
 
-for (var i = 1; i <= 10; i++) {
-  // Block Scope
+for (let i = 1; i <= 10; i++) {
+  // Block Scope will leak out the values of i as var, not let (cuz we are outside the scope where it was defined )
 }
 
 function sum(a, b) {
-  // Function Scope
+  // Function Scope, will not leak out the data outisde the scope
   var result = a + b;
 }
 
