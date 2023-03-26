81b14202f0d44d6a5599e2adad0e6351f0eeebe1
Karolina Redden <dubajkaro@gmail.com>
2023-03-26 20:52:23 -0300

    explain array module api

--- 4-modules/2-examples/2-array.js
+++ 4-modules/2-examples/2-array.js
@@ -1,3 +1,3 @@
-// Top-level API is an array
+// Top-level API is an array, here we need to use module.exports becouse by default this object is empty and in this case we are reassigning it
 
 module.exports = [2, 3, 5, 7];
