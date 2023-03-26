1a66a3d199cf6a76b817e8699603f16c6eab62f3
Karolina Redden <dubajkaro@gmail.com>
2023-03-26 20:42:27 -0300

    test module export with require in another file

--- 4-modules/1-define-use/1-index.js
+++ 4-modules/1-define-use/1-index.js
@@ -10,10 +10,9 @@
 let g = 1;//local var in a wrapping function
 // console.log(arguments)
 exports.a = 42;//first arg to a hidden wrapping function
+module.exports.b = 37;//the same as above, 
 
 console.log(arguments)
-
-
 
 //}
 
--- 4-modules/1-define-use/3-wrapper.js
+++ 4-modules/1-define-use/3-wrapper.js
@@ -1,1 +1,6 @@
-console.log(arguments);
+
+// console.log(arguments)
+exports.a = 42;//first arg to a hidden wrapping function
+module.exports.b = 37;//the same as above, 
+
+console.log(arguments)
