dde4ec2822c89c8222d6bd15a76698bd64c98b68
Karolina Redden <dubajkaro@gmail.com>
2023-03-26 20:34:55 -0300

    explian hidden node wrapping function

--- 4-modules/1-define-use/1-index.js
+++ 4-modules/1-define-use/1-index.js
@@ -1,1 +1,21 @@
-console.log('Hello Node');
+// function (arguments) {
+//this is a hidden wrappign function of node, 
+
+
+// console.log(arguments);
+//what will the above print? will print the 5 arguments which node passed to this function
+//(exports, module, require. filename, dirname), there is a hidden
+//wrapping function here
+
+let g = 1;//local var in a wrapping function
+// console.log(arguments)
+exports.a = 42;//first arg to a hidden wrapping function
+
+console.log(arguments)
+
+
+
+//}
+
+
+
