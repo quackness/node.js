5760adbb2b8392a57cdfced39e2cad7ae280b358
Karolina Redden <dubajkaro@gmail.com>
2023-03-27 12:42:22 -0300

    add another alternative with server.on

--- 5-web/1-hello-world.js
+++ 5-web/1-hello-world.js
@@ -4,7 +4,8 @@ const requestListener = (req,res) => {
   res.end('Hello World\n');
 }
 
-const server = http.createServer(requestListener);
+const server = http.createServer()
+server.on('request', requestListener);
 
 server.listen(4242, () => {
   console.log('Server is running...');
