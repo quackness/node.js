5d1f6b0e5ac7ea698ab2cfe593ffb1c2b57a2846
Karolina Redden <dubajkaro@gmail.com>
2023-03-27 12:57:41 -0300

    add console.dir to print a shallow copy of the object

--- 5-web/1-hello-world.js
+++ 5-web/1-hello-world.js
@@ -1,8 +1,12 @@
 const http = require('http');
 
 const requestListener = (req,res) => {
-  res.end('Hello World\n');//.end is a shorthand to res.write("string") + res.end()
+  console.dir(req, { depth: 1 });//instead of console.log(req) use .dir to not print nested objects 
+  // res.end('Hello World\n');//.end is a shorthand to res.write("string") + res.end()
+  res.write('Hello World\n');
+  res.end();
 }
+
 
 const server = http.createServer()
 server.on('request', requestListener);
