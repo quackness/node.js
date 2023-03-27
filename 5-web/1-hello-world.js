a729da435fb6b28bb36fd653d7cc3f5911974d58
Karolina Redden <dubajkaro@gmail.com>
2023-03-27 12:38:14 -0300

    refactor code to pass a function reference to createServer

--- 5-web/1-hello-world.js
+++ 5-web/1-hello-world.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+const requestListener = (req,res) => {
   res.end('Hello World\n');
-});
+}
+
+const server = http.createServer(requestListener);
 
 server.listen(4242, () => {
   console.log('Server is running...');
