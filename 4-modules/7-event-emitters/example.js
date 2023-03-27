fdb5f416e430d3ac2abc73cd02f12f5bc0fdcc75
Karolina Redden <dubajkaro@gmail.com>
2023-03-26 21:45:45 -0300

    add eventEmitter and listeners

--- 4-modules/7-event-emitters/example.js
+++ 4-modules/7-event-emitters/example.js
@@ -2,3 +2,12 @@ const EventEmitter = require('events');
 
 // Streams are Event Emitters
 // process.stdin, process.stdout
+
+const myEmitter = new EventEmitter();
+
+myEmitter.emit('TEST_EVENT');
+myEmitter.on('TEST_EVENT', () => {
+  console.log("Test event was fired")
+});
+
+//on will will report every time the event happens, think about it as a subscription lisnetener to an event and
