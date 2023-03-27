41c7c4d38b0a0ca907b17271ea763e3f66cf9ec0
Karolina Redden <dubajkaro@gmail.com>
2023-03-27 13:12:15 -0300

    set a get response on the root

--- 5-web/2-express/index.js
+++ 5-web/2-express/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const server = express();
 
+server.get('/', (req,res) => {
+  res.send("test")
+
+})
+
 server.listen(4242, () => {
   console.log('Express Server is running...');
 });
