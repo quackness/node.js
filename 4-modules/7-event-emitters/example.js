43a0e1ce31157620ae50bf8666845148f595e5af
Karolina Redden <dubajkaro@gmail.com>
2023-03-26 21:46:27 -0300

    move the console log code lower to see the correct output

--- 4-modules/7-event-emitters/example.js
+++ 4-modules/7-event-emitters/example.js
@@ -5,9 +5,10 @@ const EventEmitter = require('events');
 
 const myEmitter = new EventEmitter();
 
-myEmitter.emit('TEST_EVENT');
+
 myEmitter.on('TEST_EVENT', () => {
   console.log("Test event was fired")
 });
+myEmitter.emit('TEST_EVENT');
 
 //on will will report every time the event happens, think about it as a subscription lisnetener to an event and
